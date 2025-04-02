b8b703dd5666a22e0105d53256e47ca08b758497
HuuNam <truonghuunam2002@gmail.com>
2025-04-02 16:50:53 +0700

    config swagger

--- src/main/java/com/truonghuunam/backend_test__online/config/SwaggerConfig.java
+++ src/main/java/com/truonghuunam/backend_test__online/config/SwaggerConfig.java
@@ -1,18 +1,19 @@
 package com.truonghuunam.backend_test__online.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public OpenAPI simpleOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Quiz Online API")
-                        .version("1.0")
-                        .description("API docs for Quiz System"));
+                .servers(List.of(
+                        new Server().url("https://quiz-server-production-eddc.up.railway.app")));
     }
 }
