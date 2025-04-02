9b9b75359cd18f0b63de69892f4500d57bd3eb4f
HuuNam <truonghuunam2002@gmail.com>
2025-04-02 16:55:24 +0700

    config swagger

--- src/main/java/com/truonghuunam/backend_test__online/config/SecurityConfig.java
+++ src/main/java/com/truonghuunam/backend_test__online/config/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig {
                         .anyRequest().permitAll()) // Cho phép tất cả request (Dùng để test)
                 .formLogin(login -> login.disable())
                 .httpBasic(basic -> basic.disable());
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll());
 
         return http.build();
     }
--- src/main/java/com/truonghuunam/backend_test__online/config/SwaggerConfig.java
+++ src/main/java/com/truonghuunam/backend_test__online/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.truonghuunam.backend_test__online.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,12 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("Quiz API")
+                        .version("1.0")
+                        .description("API cho hệ thống thi trắc nghiệm nâng cao"))
                 .servers(List.of(
-                        new Server().url("https://quiz-server-production-eddc.up.railway.app")));
+                        new Server().url("https://quiz-server-production-eddc.up.railway.app")
+                                .description("Production Server")));
     }
 }
