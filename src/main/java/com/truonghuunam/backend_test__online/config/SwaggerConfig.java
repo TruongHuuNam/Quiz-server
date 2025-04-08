cd446957a22a401e6a4800597a07398f351519e1
HuuNam <truonghuunam2002@gmail.com>
2025-04-08 21:53:28 +0700

    delete 1 file

--- src/main/java/com/truonghuunam/backend_test__online/config/SwaggerConfig.java
+++ src/main/java/com/truonghuunam/backend_test__online/config/SwaggerConfig.java
@@ -11,15 +11,15 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Quiz API")
-                        .version("1.0")
-                        .description("API cho hệ thống thi trắc nghiệm nâng cao"))
-                .servers(List.of(
-                        new Server().url("https://quiz-server-production-eddc.up.railway.app")
-                                .description("Production Server")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Quiz API")
+                                                .version("1.0")
+                                                .description("API cho hệ thống thi trắc nghiệm nâng cao"))
+                                .servers(List.of(
+                                                new Server().url("https://quiz-server-production-eddc.up.railway.app")
+                                                                .description("Production Server")));
+        }
 }
